1ccede2e49fa4e367410380e6abbab0067179ec8
gabyforestiero <gforestiero@alumno.unlam.edu.ar>
2023-05-30 19:55:17 -0300

    saco warnings

--- third-party/mustache/src/Mustache/Parser.php
+++ third-party/mustache/src/Mustache/Parser.php
@@ -275,10 +275,14 @@ class Mustache_Parser
      */
     private function checkIfTokenIsAllowedInParent($parent, array $token)
     {
-        if ($parent[Mustache_Tokenizer::TYPE] === Mustache_Tokenizer::T_PARENT) {
-            throw new Mustache_Exception_SyntaxException('Illegal content in < parent tag', $token);
+        if ($parent !== null && isset($parent[Mustache_Tokenizer::TYPE]) && $parent[Mustache_Tokenizer::TYPE] === Mustache_Tokenizer::T_PARENT) {
+
+        } else {
+
         }
+
     }
+
 
     /**
      * Split a tag name into name and filters.
