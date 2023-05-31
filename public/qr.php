844749d23f039f509e7101c0d5a5fc3c47b5ef88
Nahue <narolon@alumno.unlam.edu.ar>
2023-05-30 21:56:24 -0300

    mini error solucionado

--- public/qr.php
+++ public/qr.php
@@ -5,7 +5,7 @@ require "../third-party/phpqrcode/qrlib.php";
 $level = "Q";
 $tamaño = 4;
 $framSize = 3;
-$ruta="https//:localhost/perfil/perfil?idUsuario=".$_GET['id'];
+$ruta="https//:localhost/perfil/perfil&idUsuario=".$_GET['id'];
 QRcode::png($ruta, null, $level, $tamaño, $framSize);
 
 ?>
