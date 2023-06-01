d8b9a22123a86e0308f2552bbedb49b72254a267
jazmin <jazminisabeldestefano@gmail.com>
2023-06-01 20:14:03 -0300

    unificado de estilos

--- public/qr.php
+++ public/qr.php
@@ -3,8 +3,8 @@ require "../third-party/phpqrcode/qrlib.php";
 
 
 $level = "Q";
-$tamaño = 4;
-$framSize = 3;
+$tamaño = 8;
+$framSize = 2;
 $ruta="https//:localhost/perfil/perfil&idUsuario=".$_GET['id'];
 QRcode::png($ruta, null, $level, $tamaño, $framSize);
 
