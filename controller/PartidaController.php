ccf444169faa4e8930582a19bf4d94e755a8ce6d
Jazmin De Stéfano <jazmindestefano@MacBook-Pro-de-Tecnico.local>
2023-06-03 00:39:08 -0300

    añadir jugar partida de nahue y markos

--- controller/PartidaController.php
+++ controller/PartidaController.php
@@ -12,9 +12,18 @@ class PartidaController
         $this->renderer = $renderer;
     }
 
-    public function jugar()
+    public function ver()
     {
-        $data = [];
+        $users = $this->partidaModel->getUsers();
+        $data = array('users' => $users);
+        $this->renderer->render('partida', $data);
+    }
+
+    public function empezar(){
+        $cantidadDePreguntas= Count($this->partidaModel->getCantidadDePreguntas());
+        $id= rand(1, $cantidadDePreguntas);
+        $pregunta = $this->partidaModel->getPregunta($id);
+        $data = array('preguntas'=>$pregunta) ;
         $this->renderer->render('partida', $data);
     }
 }
--- model/PartidaModel.php
+++ model/PartidaModel.php
@@ -1,11 +1,28 @@
 <?php
 
+
 class PartidaModel
 {
+
+
     private $database;
 
     public function __construct($database)
     {
         $this->database = $database;
+    }
+
+
+    public function getCantidadDePreguntas()
+    {
+        $query = "SELECT Pregunta FROM preguntas";
+        return $this->database->query($query);
+    }
+
+
+    public function getPregunta($id)
+    {
+        $query = "SELECT Pregunta FROM preguntas WHERE ID= '$id' ";
+        return $this->database->query($query);
     }
 }
