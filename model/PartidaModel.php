dc566592bf91a017a04490afcdfd82c99ce5c5fc
Luca Bernacchia <lucabernacchia98@gmail.com>
2023-06-03 11:39:12 -0300

    mostrar pregunta con sus respuestas

--- model/PartidaModel.php
+++ model/PartidaModel.php
@@ -19,10 +19,14 @@ class PartidaModel
         return $this->database->query($query);
     }
 
-
     public function getPregunta($id)
     {
-        $query = "SELECT Pregunta FROM preguntas WHERE ID= '$id' ";
+        $query = "SELECT * FROM preguntas WHERE ID= '$id' ";
         return $this->database->query($query);
     }
+
+		public function getPreguntaCorrectaByIdDePregunta($id) {
+			$query = "SELECT RespuestaCorrecta FROM preguntas WHERE ID='$id'";
+			return $this->database->query($query);
+		}
 }
