7485f6539e8c3ad8f1adc9efd3a64c67a5feb881
Nahue <narolon@alumno.unlam.edu.ar>
2023-06-03 21:50:08 -0300

    Preguntas funcionando

--- controller/PartidaController.php
+++ controller/PartidaController.php
@@ -23,27 +23,33 @@
         {
             $cantidadDePreguntas = Count($this->partidaModel->getCantidadDePreguntas());
             $id = rand(1, $cantidadDePreguntas);
+
             $pregunta = $this->partidaModel->getPregunta($id);
-            $respuestaCorrecta = $this->partidaModel->getPreguntaCorrectaByIdDePregunta($id)[0]['RespuestaCorrecta'];
-            $respuestaDelUsuario = $_POST['respuestaDelUsuario'];
-
-            $mensaje = $this->partidaModel->respuestaMensaje($respuestaCorrecta, $respuestaDelUsuario);
-
-
-            if ($respuestaDelUsuario == $respuestaCorrecta) {
-                $data = array('preguntas' => $pregunta,
-                    'mensajeDeLaPartida' => $mensaje);
-
-            } else {
-                $data = array('preguntas' => $pregunta);
-
-            }
+            $respuesta = $this->partidaModel->getRespuesta($id);
+            $data = array('preguntas' => $pregunta,
+                'respuestas' => $respuesta );
             $this->renderer->render('partida', $data);
-
         }
 
         public function validar()
         {
+            $idDePregunta= $this->partidaModel->getPreguntaByIdRespuesta($_GET['id'])[0]['idPregunta'];
+            $respuestaDelUsuario= $this->partidaModel->getRespuestaPorId($_GET['id'])[0]['respuesta'];
+            $respuestaCorrecta = $this->partidaModel->getRespuestaCorrectaByIdDePregunta($idDePregunta)[0]['respuesta'];
+            $mensaje = $this->partidaModel->respuestaMensaje($respuestaCorrecta, $respuestaDelUsuario);
+            $pregunta = $this->partidaModel->getPregunta($idDePregunta);
+
+            if ($respuestaDelUsuario == $respuestaCorrecta) {
+                $data = array('preguntas' => $pregunta,
+                    'mensajeDeLaPartida' => $mensaje);
+                header('location: /partida/empezar');
+
+            } else {
+                $data = array('preguntas' => $pregunta,
+                    'mensajeDeLaPartida' => $mensaje);
+                $this->renderer->render('partida', $data);
+
+            }
 
         }
 
--- model/PartidaModel.php
+++ model/PartidaModel.php
@@ -15,19 +15,19 @@
 
         public function getCantidadDePreguntas()
         {
-            $query = "SELECT Pregunta FROM preguntas";
+            $query = "SELECT pregunta FROM preguntas";
             return $this->database->query($query);
         }
 
         public function getPregunta($id)
         {
-            $query = "SELECT * FROM preguntas WHERE ID= '$id' ";
+            $query = "SELECT * FROM preguntas WHERE idPregunta= '$id' ";
             return $this->database->query($query);
         }
 
-        public function getPreguntaCorrectaByIdDePregunta($id)
+        public function getRespuestaCorrectaByIdDePregunta($id)
         {
-            $query = "SELECT RespuestaCorrecta FROM preguntas WHERE ID= '$id' ";
+            $query = "SELECT respuesta FROM respuestas WHERE isCorrecta= 1 AND idPregunta= '$id' ";
             return $this->database->query($query);
         }
 
@@ -42,5 +42,23 @@
             return $data;
         }
 
+
+        public function getRespuesta($idPregunta)
+        {
+            $query = "SELECT * FROM respuestas WHERE idPregunta= '$idPregunta' ";
+            return $this->database->query($query);
+        }
+
+        public function getRespuestaPorId($idRespuesta)
+        {
+            $query = "SELECT respuesta FROM respuestas WHERE idRespuesta= '$idRespuesta' ";
+            return $this->database->query($query);
+        }
+
+        public function getPreguntaByIdRespuesta($id)
+        {
+            $query = "SELECT idPregunta FROM respuestas WHERE  idRespuesta= '$id' ";
+            return $this->database->query($query);
+        }
 
     }
