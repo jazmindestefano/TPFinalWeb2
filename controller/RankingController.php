e559f977226545ad6887f085b20a55cae1f2a072
Luca Bernacchia <lucabernacchia98@gmail.com>
2023-06-07 15:05:39 -0300

    filtro de ranking con AJAX

--- controller/RankingController.php
+++ controller/RankingController.php
@@ -1,29 +1,37 @@
 <?php
 
-    class RankingController
-    {
-        private $rankingModel;
-        private $renderer;
+	class RankingController
+	{
+		private $rankingModel;
+		private $renderer;
 
-        public function __construct(RankingModel $rankingModel, MustacheRender $renderer)
-        {
-            $this->rankingModel = $rankingModel;
-            $this->renderer = $renderer;
-        }
-
-        public function ver()
-        {
-	        $filtro = isset($_POST['filtro']) ? $_POST['filtro'] : 'desc';
+		public function __construct(RankingModel $rankingModel, MustacheRender $renderer)
+		{
+			$this->rankingModel = $rankingModel;
+			$this->renderer = $renderer;
+		}
 
 
+		public function ver()
+		{
+			$users = $this->rankingModel->getUsersByOrderDesc();
 
-						if($filtro == 'desc') {
-							$users = $this->rankingModel->getUsersByOrderDesc();
-						} else {
-							$users = $this->rankingModel->getUsersByOrderAsc();
-						}
+			$data = array('users' => $users);
+			$this->renderer->render('ranking', $data);
+		}
 
-            $data = array('users' => $users);
-            $this->renderer->render('ranking', $data);
-        }
-    }
+		public function filtrar()
+		{
+			$filtro = $_GET['filtro'] ?? 'desc';
+			$users = $filtro == 'desc' ? $this->rankingModel->getUsersByOrderDesc() : $this->rankingModel->getUsersByOrderAsc();
+			$response = ['users' => $users];
+
+			header('Content-Type: application/json');
+			echo json_encode($response);
+		}
+
+
+
+
+
+	}
