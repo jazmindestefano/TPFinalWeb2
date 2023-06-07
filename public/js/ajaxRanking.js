b06d02c4caebc0d5bc7dd9dff167899779676f78
Luca Bernacchia <lucabernacchia98@gmail.com>
2023-06-07 15:10:16 -0300

    modularizar en carpeta JS

---
+++ public/js/ajaxRanking.js
@@ -0,0 +1,29 @@
+function filtrar() {
+    let filtro = $("#filtro").val();
+    if (filtro) {
+        $.ajax({
+            url: 'http://localhost/ranking/filtrar',
+            method: "GET",
+            data: { filtro: filtro },
+            success: function(response) {
+
+
+
+                $("#ranking-table tbody").empty();
+
+
+                response.users.forEach(function(user) {
+                    let fila = `<tr>
+                                <td>${user.posicion}</td>
+                                <td><a href="/perfil/perfil&idUsuario=${user.idUsuario}">${user.username}</a></td>
+                                <td>${user.puntaje}</td>
+                            </tr>`;
+                    $("#ranking-table tbody").append(fila);
+                });
+            },
+            error: function() {
+                alert("Error");
+            }
+        });
+    }
+}
