e6e5687df428a76fcfa67cd9bfc0bdfb3020436f
jazmin <jazminisabeldestefano@gmail.com>
2023-06-13 21:50:48 -0300

    contador no se reinicia

--- public/js/cronometro.js
+++ public/js/cronometro.js
@@ -5,20 +5,67 @@ document.addEventListener("DOMContentLoaded", function() {
     contadorElemento.id = "contador";
     preguntaElemento.insertAdjacentElement('afterend', contadorElemento);
 
-    contarAtras();
+    const tiempoRestante = obtenerTiempoRestante();
+    if (tiempoRestante > 0) {
+        contarAtras(tiempoRestante);
+    } else {
+        iniciarNuevoContador();
+    }
+
+    const opcionesRespuesta = document.querySelectorAll(".opcion-respuesta");
+    opcionesRespuesta.forEach(opcion => {
+        opcion.addEventListener("click", verificarRespuesta);
+    });
 });
 
-function contarAtras() {
+function obtenerTiempoRestante() {
+    const tiempoGuardado = localStorage.getItem("tiempoRestante");
+    return tiempoGuardado ? parseInt(tiempoGuardado) : 0;
+}
+
+function guardarTiempoRestante(tiempoRestante) {
+    localStorage.setItem("tiempoRestante", tiempoRestante.toString());
+}
+
+function contarAtras(segundos) {
     const contador = document.getElementById("contador");
-    let segundos = 10;
+    let tiempoRestante = segundos;
 
     const intervalo = setInterval(() => {
-        if (segundos > 0) {
-            contador.textContent = segundos;
-            segundos--;
+        if (tiempoRestante > 0) {
+            contador.textContent = tiempoRestante;
+            tiempoRestante--;
         } else {
             contador.textContent = "¡Tiempo terminado!";
             clearInterval(intervalo);
+           // redirigir();
         }
+        guardarTiempoRestante(tiempoRestante);
     }, 1000);
 }
+
+function iniciarNuevoContador() {
+    guardarTiempoRestante(10); // Establece el tiempo inicial en 10 segundos
+    contarAtras(10);
+}
+
+function redirigir() {
+    localStorage.removeItem("tiempoRestante"); // Elimina el tiempo restante al finalizar el contador
+    window.location.href = "https://www.ejemplo.com"; // Reemplaza la URL con la que desees redirigir
+}
+
+function verificarRespuesta(event) {
+
+    /*
+    const respuestaCorrecta = false; // Reemplaza con tu lógica de verificación de respuesta
+
+    if (!respuestaCorrecta) {
+        const tiempoRestante = obtenerTiempoRestante();
+        contarAtras(tiempoRestante);
+    }
+     */
+console.log('verificar respuesta');
+
+    // Resto de la lógica de verificación y acciones correspondientes
+}
+
